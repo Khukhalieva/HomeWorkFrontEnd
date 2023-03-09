6148265ca06d24c1121c0d4716d0fba49a5ab705
Tatiana Khukhalieva <Tatiana>
2023-03-09 13:55:43 +0200

    Homework2

---
+++ hw2Khukhalieva/index.js
@@ -0,0 +1,19 @@
+let number1 = prompt('Hi, please enter one number.');
+let number2 = prompt('Enter another number.');
+
+let sum =  +number1 + +number2;
+let sumString = String( sum );
+console.log( sumString );
+
+let diff = number1 - number2;
+let diffString = String( diff );
+console.log( diffString);
+
+let mult = number1 * number2;
+let multString = String( mult );
+console.log( multString);
+
+let div = number1 / number2;
+let divString = String( div );
+console.log( divString);
+
