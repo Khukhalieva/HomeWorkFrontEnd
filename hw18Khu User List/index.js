3eb95d0b8aed5044c0e6b247719b3881f6a61098
Tatiana Khukhalieva <Tatiana>
2023-06-22 21:17:57 +0300

    add function HW 18 User List

--- hw18Khu User List/index.js
+++ hw18Khu User List/index.js
@@ -6,6 +6,16 @@ const userList = document.getElementById('user-list');
 
 const xhr = new XMLHttpRequest();
 
+function prepareUserCard(user, cardTemplate) {
+    // присваивание переменной currentUserList значения шаблона карточки
+    let currentUserList = cardTemplate;
+    //перебор всех ключей объекта user с помощью метода forEach
+    Object.key(user).forEach(key  => {
+        // Замена всех вхождений '{{key}}' в шаблоне карточки на соответствующие значения из объекта user
+        currentUserList = currentUserList.replaceAll('{{key}}', user[key]);
+    });
+    return currentUserList;
+}
 function renderUserList(users)   {
 //получаем единое поле 'name', с помощью создания обьекта map
     const mappedUsers = users.map( user => {
@@ -23,18 +33,16 @@ function renderUserList(users)   {
         //     .replaceAll('{{name}}', user.name)
         //     .replaceAll('{{email}}', user.email)
         //     .replaceAll('{{avatar}}', user.avatar);
-        let currentUserList = '';
-        //получение ключей обьекта юзер
-        Object.key(user).forEach(key  => {
-            currentUserList = cardTemplate.replaceAll()
-        });
 
+        // добавление текущей карточки пользователя к общему результату списка пользователей
+        userListResult += prepareUserCard(user, cardTemplate);
     });
 
+    // вывод обновленного списка пользователей на страницу
     userList.innerHTML = userListResult;
 }
 //создаем функцию, в которой будет выполняться сетевой запрос для получения данных о пользователях
-function fetchUsers ()  {
+function fetchUsers (callback)  {
     //синхронній сетевой GET-запрос к URL
     xhr.open('GET', 'https://reqres.in/api/users?page=1', true);
     //настройка формата json
@@ -47,7 +55,7 @@ function fetchUsers ()  {
             // пытаемся разобрать ответ сервера в формате JSON
             const response = JSON.parse(e.target.response);
             //вызов функции создания карточки пользователя
-            renderUserList(response.data);
+            callback(response.data);
 
         } catch (error)   {
             console.warn('Error parsing JSON');
@@ -60,7 +68,16 @@ function fetchUsers ()  {
     // Отправляем сетевой запрос
     xhr.send();
 }
-// Вызываем функцию fetchUsers для получения данных о пользователях
-fetchUsers();
+    // самовызывающаяся функция
+    (()=>{
+        // вызов функции fetchUsers с колбэком, который будет вызван после получения списка пользователей
+        fetchUsers((users) => {
+            // вызов функции renderUserList для отображения списка пользователей
+            renderUserList(users);
+        });
+
+
+    })();
+
 
 
